2f46ee6cd51acf5a383e218a980133bba15212c6
pxysource <panxingyuan1@163.com>
2025-10-20 10:36:05 +0800

    [SimpleFactory] Create FactoryMethod project.

---
+++ FactoryMethod/main.cpp
@@ -0,0 +1,87 @@
+//
+// Created by pxy on 2025/10/17.
+//
+
+#include <iostream>
+#include <utility>
+
+using std::cout;
+using std::endl;
+
+class Product
+{
+protected:
+	std::string Name;
+
+public:
+	explicit Product(std::string name) : Name(std::move(name))
+	{
+	}
+	virtual ~Product() = default;
+
+	virtual void show() const
+	{
+		cout << "Product: " << Name << endl;
+	}
+};
+
+class ConcreteProductA final : public Product
+{
+public:
+	explicit ConcreteProductA(std::string name) : Product(std::move(name))
+	{
+		cout << "Create ConcreteProductA: " << Name << endl;
+	}
+};
+
+class ConcreteProductB final : public Product
+{
+public:
+	explicit ConcreteProductB(std::string name) : Product(std::move(name))
+	{
+		cout << "Create ConcreteProductB: " << Name << endl;
+	}
+};
+
+class Factory
+{
+public:
+	Factory() = default;
+	virtual ~Factory() = default;
+	virtual Product* CreateProduct() = 0;
+};
+
+class ConcreteFactoryA final : public Factory
+{
+public:
+	Product* CreateProduct() override
+	{
+		return new ConcreteProductA("PA");
+	}
+};
+
+class ConcreteFactoryB final : public Factory
+{
+public:
+	Product* CreateProduct() override
+	{
+		return new ConcreteProductB("PB");
+	}
+};
+
+int main()
+{
+	const auto factoryA = new ConcreteFactoryA();
+	const auto factoryB = new ConcreteFactoryB();
+	const auto productA = factoryA->CreateProduct();
+	const auto productB = factoryB->CreateProduct();
+	productA->show();
+	productB->show();
+
+	delete productA;
+	delete productB;
+	delete factoryA;
+	delete factoryB;
+
+	return 0;
+}
